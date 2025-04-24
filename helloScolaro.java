62cb1077b683d72b34db0eb4143800969a4c4553
Edo-oss <edoardo.scolaro@capellinisauro.edu.it>
2025-04-24 07:37:22 +0000

    Stampo nome

--- helloScolaro.java
+++ helloScolaro.java
@@ -1,5 +1,13 @@
 public class helloScolaro{
     public static void main(String[] args) {
         System.out.println("Hello Edoardo");
-    }
+    
+
+
+for(int i=0;i<3;i++){
+    System.out.println("Edoardo Scolaro");
+    
+}
+
+}
 }
