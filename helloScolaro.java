8cc2c5b15aaa82b8333a5fe720a3034fc00251ab
Edo-oss <edoardo.scolaro@capellinisauro.edu.it>
2025-05-08 06:53:02 +0000

    Aggiunta stampa da ARRAY STATICO

--- helloScolaro.java
+++ helloScolaro.java
@@ -14,4 +14,17 @@ for(int i=0;i<3;i++){
 
 //Stampa la data di nascita
 System.out.println("13 novembre 2007");
- 
+ //Stampa "Forza Squadra"
+ System.ou.println("Forza Juve");
+
+ public class hello{
+    public static void main(String[] args) {
+        System.out.println(" Hello Scolaro");
+        //array di carattere con le lettere del nome "Edoardo"
+        char[] lettere = {'E','d','o','a','r','d','o'};
+        //stampo le lettere dell'array
+        for ( char lettera:lettere){
+            System.out.println(lettera + "");
+        }
+    }
+ }
