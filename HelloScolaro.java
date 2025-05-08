71a52fa7d5dc37ba3a769ab1c5311beb413c1c04
Edo-oss <edoardo.scolaro@capellinisauro.edu.it>
2025-05-08 07:18:40 +0000

    aggiunta stampa array

---
+++ HelloScolaro.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HelloScolaro {
+    public static void main(String[] args) {
+        System.out.println("Hello Scolaro!");
+
+        char[] lettere = {'E', 'D', 'O'};
+        for (char lettera : lettere) {
+            System.out.println(lettera);
+        }
+        
+    }
+}
