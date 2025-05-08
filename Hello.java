6dd6949f10721dc82861c44ca4cda1b819c7db87
Edo-oss <edoardo.scolaro@capellinisauro.edu.it>
2025-05-08 07:25:29 +0000

    Aggiunta stampa da Vector

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello Edo");
+        vector<Character> lettre = new vector<>();
+        lettere.add('E');
+        lettere.add('d');
+        lettere.add('o');
+        +
+        for (Character lettera : lettere){
+            System.out.println(lettera);
+        }
+    }
+}
